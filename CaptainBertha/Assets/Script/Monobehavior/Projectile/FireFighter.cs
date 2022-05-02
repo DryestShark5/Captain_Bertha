32df95229382df0d67ce73a5621dca21bea23ca1
Håvard Fiskum <haavard.f.123@live.no>
2022-05-02 16:18:29 +0200

    Siste Push?

--- CaptainBertha/Assets/Script/Monobehavior/Projectile/FireFighter.cs
+++ CaptainBertha/Assets/Script/Monobehavior/Projectile/FireFighter.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FireFighter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float timeToBurn = 10f;
+
+    private void Start()
     {
-        
+        Destroy(gameObject, timeToBurn);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
